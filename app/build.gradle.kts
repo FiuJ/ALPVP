9baa867d865237dd142a1d06f12d15d839d2593e
Jevon <winartojevon@gmail.com>
2024-12-26 14:19:07 +0700

    Login View
    Login View kecuali button sign up with Google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.alpvp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.alpvp"
---
+++ app/src/main/java/com/example/alpvp/View/Login.kt
@@ -0,0 +1,196 @@
+package com.example.alpvp.View
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.alpvp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFE9602A)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.login_image),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .height(260.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Column (
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ){
+            Text(
+                text = "Make yourself known!",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextField(
+                value = "",
+                onValueChange = { },
+                label = {
+                    Text("Username / Email Address", color = Color.Gray)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Black
+                )
+
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            TextField(
+                value = "",
+                onValueChange = { },
+                label = {
+                    Text("Password", color = Color.Gray)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Black
+                )
+
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    containerColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 20.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Divider(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .weight(1f),
+                    color = Color.DarkGray
+                )
+                Text(
+                    text = "or",
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .weight(0.5f),
+                    textAlign = TextAlign.Center,
+                    color = Color.DarkGray
+                )
+                Divider(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .weight(1f),
+                    color = Color.DarkGray
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    Text(
+                        text = "Dont have an account? ",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color.White
+                    )
+                    Text(
+                        text = "Sign Up",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier
+                            .clickable {
+                                //ke Sign Up
+                            }
+                    )
+                }
+
+
+            }
+
+
+            
+        }
+
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun LoginPreview(){
+    Login()
+}
